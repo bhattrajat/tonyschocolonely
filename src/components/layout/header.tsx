5a274b1ed2b9d53cd883b87b41361541902746b5
Rajat Bhatt <rajatbhatt999@gmail.com>
2023-10-12 13:40:54 -0400

    Add link to homepage on header logo

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image';
-import Cart from '../ui/cart';
+import Image from "next/image";
+import Cart from "../ui/cart";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-orange-500 p-4 flex justify-between md:px-8">
-      <Image src="/logo.webp" alt="company logo" width={100} height={100} />
+    <header className="flex justify-between bg-orange-500 p-4 md:px-8">
+      <Link href="/">
+        <Image src="/logo.webp" alt="company logo" width={100} height={100} />
+      </Link>
       <Cart />
     </header>
   );
